528b22f05de040b2bc7202d2f88da91010cc544a
Dalena <l.dalena59@gmail.com>
2020-06-30 19:38:00 -0700

    Added shadows to cards

--- pokedex/src/components/pokemon/PokeCard.js
+++ pokedex/src/components/pokemon/PokeCard.js
@@ -6,6 +6,14 @@ const Sprite = styled.img `
     height: 5em; 
 `;
 
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover{
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);  
+    }
+`; 
+
 // Card represents a single pokemon
 export default class PokeCard extends Component {
     state = {
@@ -30,7 +38,7 @@ export default class PokeCard extends Component {
 
         return (
             <div className="col-md-3 cold-sm-6 mb-5">
-                <div className="card">
+                <Card className="card">
                     <h5 className="card-header"><h1>{this.state.pokemonId}</h1></h5>
                     <Sprite className="card-img-top mx-auto mt-2" 
                     src={this.state.image}>
@@ -39,7 +47,7 @@ export default class PokeCard extends Component {
                         <h6 className="card-title">{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase()+ letter.substring(1))
                         .join(' ')}</h6>
                     </div>
-                </div>
+                </Card>
             </div>
         )
     }
