6905893ee1c3153de4e192e1bcca79d033d5e02f
Dalena <l.dalena59@gmail.com>
2020-06-30 19:19:59 -0700

    Formatted cards, added sprites for most Johto pokemon

--- pokedex/src/components/pokemon/PokeCard.js
+++ pokedex/src/components/pokemon/PokeCard.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'; 
+import styled from 'styled-components';
 
+const Sprite = styled.img `
+    width: 5em;
+    height: 5em; 
+`;
 
 // Card represents a single pokemon
 export default class PokeCard extends Component {
@@ -9,15 +14,30 @@ export default class PokeCard extends Component {
         pokemonId: ''
     }; 
 
+    componentDidMount(){
+        const {name, url} = this.props;
+        const pokemonId = url.split("/")[url.split("/").length - 2]; 
+        const image = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonId}.png?raw=true`
+    
+        this.setState({
+            name, 
+            image, 
+            pokemonId
+        }); 
+    }
+
     render() {
-        const name = this.props.name; 
-        const url = this.props.url;
 
         return (
             <div className="col-md-3 cold-sm-6 mb-5">
                 <div className="card">
-                    <div className="card-header">
-                        <h1>{name}</h1>
+                    <h5 className="card-header"><h1>{this.state.pokemonId}</h1></h5>
+                    <Sprite className="card-img-top mx-auto mt-2" 
+                    src={this.state.image}>
+                    </Sprite>
+                    <div className="card-body mx-auto">
+                        <h6 className="card-title">{this.state.name.toLowerCase().split(" ").map(letter => letter.charAt(0).toUpperCase()+ letter.substring(1))
+                        .join(' ')}</h6>
                     </div>
                 </div>
             </div>
